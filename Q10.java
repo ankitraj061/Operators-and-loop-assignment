03ed1ed3f39c7866cc2a5c2a82ee264a8bac517c
Ankit Raj <116249018+ankitraj061@users.noreply.github.com>
2023-05-24 10:04:23 +0530

    Update and rename Q18.java to Q10.java

--- Q10.java
+++ Q10.java
@@ -1,6 +1,6 @@
 //write a program to print the number between 1 to 10.
 
-public class Q18 {
+public class Q10 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
