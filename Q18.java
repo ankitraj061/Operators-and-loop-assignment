a8973b62a60081db0bd33918727be8f4eb1e8dbf
Ankit Raj <116249018+ankitraj061@users.noreply.github.com>
2023-05-24 10:03:37 +0530

    Add files via upload

---
+++ Q18.java
@@ -0,0 +1,11 @@
+//write a program to print the number between 1 to 10.
+
+public class Q18 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
